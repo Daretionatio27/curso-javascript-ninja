1ac489ce72c63398ab0cfa82366a23ae64668abb
Daretionatio27 <reginamaia.maia22@gmail.com>
2025-04-21 18:08:35 -0300

    Update challenge-27.js

--- challenge-27/challenge-27.js
+++ challenge-27/challenge-27.js
@@ -1,21 +1,119 @@
-/*
-Aproveitando a lib DOM que fizemos na semana anterior, crie agora para ela
-métodos semelhantes aos que existem no array, mas que sirvam para os
-elementos do DOM selecionados.
-Crie os seguintes métodos:
-- forEach, map, filter, reduce, reduceRight, every e some.
+function DOM(selector) {
+  this.element = document.querySelectorAll(selector);
+}
 
-Crie também métodos que verificam o tipo do objeto passado por parâmetro.
-Esses métodos não precisam depender de criar um novo elmento do DOM, podem
-ser métodos estáticos.
+DOM.prototype.on = function on(eventType, callback) {
+  this.element.forEach(function(element) {
+    element.addEventListener(eventType, callback, false);
+  });
+};
 
-Métodos estáticos não obrigam o uso do `new`, podendo ser usados diretamente
-no objeto, como nos exemplos abaixo:
-DOM.isArray([1, 2, 3]); // true
-DOM.isFunction(function() {}); // true
-DOM.isNumber('numero'); // false
+DOM.prototype.off = function off(eventType, callback) {
+  this.element.forEach(function(element) {
+    element.removeEventListener(eventType, callback, false);
+  });
+};
 
-Crie os seguintes métodos para verificação de tipo:
-- isArray, isObject, isFunction, isNumber, isString, isBoolean, isNull.
-O método isNull deve retornar `true` se o valor for null ou undefined.
-*/
+DOM.prototype.get = function get() {
+  return this.element;
+};
+
+DOM.prototype.forEach = function forEach() {
+  Array.prototype.forEach.apply(this.element, arguments);
+};
+
+DOM.prototype.map = function map() {
+  return Array.prototype.map.apply(this.element, arguments);
+};
+
+DOM.prototype.filter = function filter() {
+  return Array.prototype.filter.apply(this.element, arguments);
+};
+
+DOM.prototype.reduce = function reduce() {
+  return Array.prototype.reduce.apply(this.element, arguments);
+};
+
+DOM.prototype.reduceRight = function reduceRight() {
+  return Array.prototype.reduceRight.apply(this.element, arguments);
+};
+
+DOM.prototype.every = function every() {
+  return Array.prototype.every.apply(this.element, arguments);
+};
+
+DOM.prototype.some = function some() {
+  return Array.prototype.some.apply(this.element, arguments);
+};
+
+DOM.isArray = function isArray(obj) {
+  return Object.prototype.toString.call(obj) === '[object Array]';
+};
+
+DOM.isObject = function isObject(obj) {
+  return Object.prototype.toString.call(obj) === '[object Object]';
+};
+
+DOM.isFunction = function isFunction(obj) {
+  return Object.prototype.toString.call(obj) === '[object Function]';
+};
+
+DOM.isNumber = function isNumber(obj) {
+  return Object.prototype.toString.call(obj) === '[object Number]';
+};
+
+DOM.isString = function isString(obj) {
+  return Object.prototype.toString.call(obj) === '[object String]';
+};
+
+DOM.isBoolean = function isBoolean(obj) {
+  return Object.prototype.toString.call(obj) === '[object Boolean]';
+};
+
+DOM.isNull = function isNull(obj) {
+  return obj === null || obj === undefined;
+};
+
+// Código de teste (para verificar os métodos criados)
+(function() {
+  var $links = new DOM('[data-js="link"]');
+
+  $links.forEach(function(link, index) {
+    console.log('forEach:', link, index);
+  });
+
+  var hrefs = $links.map(function(link) {
+    return link.getAttribute('href');
+  });
+  console.log('map:', hrefs);
+
+  var linksComHrefVazio = $links.filter(function(link) {
+    return link.getAttribute('href') === '';
+  });
+  console.log('filter:', linksComHrefVazio);
+
+  var quantidadeDeLinks = $links.reduce(function(accumulator, link) {
+    return accumulator + 1;
+  }, 0);
+  console.log('reduce:', quantidadeDeLinks);
+
+  var algumLinkExiste = $links.some(function(link) {
+    return link.textContent.includes('2');
+  });
+  console.log('some:', algumLinkExiste);
+
+  var todosLinksSaoElementosA = $links.every(function(link) {
+    return link.tagName === 'A';
+  });
+  console.log('every:', todosLinksSaoElementosA);
+
+  console.log('isArray:', DOM.isArray([1, 2, 3]));
+  console.log('isObject:', DOM.isObject({ a: 1 }));
+  console.log('isFunction:', DOM.isFunction(function() {}));
+  console.log('isNumber:', DOM.isNumber(123));
+  console.log('isString:', DOM.isString('hello'));
+  console.log('isBoolean:', DOM.isBoolean(true));
+  console.log('isNull (null):', DOM.isNull(null));
+  console.log('isNull (undefined):', DOM.isNull(undefined));
+  console.log('isNull (false):', DOM.isNull(false));
+})();
